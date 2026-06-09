bc611e188cc72149be72586f3a22af2abdb80272
ISHWARz <ishwaranpat261@gmail.com>
2026-06-09 21:24:35 +0530

    Update Form1.cs

--- RailEase/Form1.cs
+++ RailEase/Form1.cs
@@ -7,24 +7,5 @@ namespace RailEase
             InitializeComponent();
         }
 
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void label4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pnlSearchCard_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void label10_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
